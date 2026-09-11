f835fe1708bdd6b76c859bd750a956bc8ec1fd77
Ádám <adamsimon2332@gmail.com>
2026-09-11 19:36:31 +0200

    chore(config): drop dead phone provider key

--- settings/sharedConfig.lua
+++ settings/sharedConfig.lua
@@ -3,7 +3,7 @@ BridgeSharedConfig.Lang          = "auto"        -- Change this to your preferre
 BridgeSharedConfig.DebugLevel    = 0             -- Change this to your Debug level, 0 for none, 1 for some, 2 for all data
 BridgeSharedConfig.Notify        = "auto"        -- [ auto | ox_lib | r_notify | wasabi_notify | lation_ui | ZSX_UIV2 | okokNotify | mythic_notify | pNotify | t-notify | brutal_notify | FL-Notify]
 BridgeSharedConfig.HelpText      = "auto"        -- [ auto | ox_lib | jg-textui | lation_ui | ZSX_UIV2 | cd_drawtextui | lab-HintUI | okokTextUI]
-BridgeSharedConfig.Phone         = "auto"        -- [ auto | qs-smartphone | lb-phone | gksphone]
+-- Phone provider is detected from started resources (see modules/phone).
 BridgeSharedConfig.Skills        = "auto"        -- [ auto | OT_skills | evolent_skills | pickle_xp]
 
 return BridgeSharedConfig
